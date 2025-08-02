616548f345f71c8b603840bfe401398939412265
Venkata Rohit Potnuru <117942412+potnururohit@users.noreply.github.com>
2025-08-02 19:06:03 +0530

    Add files via upload

---
+++ portfolio-site-rohit/src/index.js
@@ -0,0 +1,8 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Portfolio from "./Portfolio";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Portfolio />);
